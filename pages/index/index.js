3a8182fdc608dc2bd84aa83d9be6a79c1fc7b9ba
STAFF\zhangwei36 <1004695936@qq.com>
2017-10-26 17:31:35 +0800

    进入小程序的广告位

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,76 +17,65 @@ Page({
     // 轮播间隔
     interval: 5000,
     // 单图滚动时长
-    duration: 1000,
+    duration: 800,
     // 是否开启无缝滚动
     circular: false,
     // 指示点颜色
     indicatorColor:'#fff',
     // 当前选中的指示点的颜色
-    indicatorActiveColor:'#00aebe',
+    indicatorActiveColor:'#00d6ff',
     // 按钮的class动态绑定，一定要用setData方法进行动态绑定
     btnClass:'hidden-btn',
-
-    motto: 'WelCome',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 轮播图文字
+    bannerText:'Vue',
   },
   // 轮播图改变的时候触发的回调
   bannerChange:function(e){
     // 输出第一个参数可以看到具体的信息
-    console.log(e.detail.current)
-    if (e.detail.current==3){
-      this.setData({
-        btnClass: 'start-btn'
-      })
-    }else{
-      this.setData({
-        btnClass: 'hidden-btn'
-      })
+    // console.log(e.detail.current)
+    // 轮播到最后一个显示按钮
+    e.detail.current == 3 ? this.setData({
+      btnClass: 'start-btn'
+    }) : this.setData({
+      btnClass: 'hidden-btn'
+    })
+    // 改变对应轮播图的内容
+    this.changeText(e.detail.current)
+  },
+  // 控制文字内容
+  changeText:function(i){
+    switch (i) {
+      case 0:
+        this.setData({
+          bannerText: 'VUE'
+        })
+        break;
+      case 1:
+        this.setData({
+          bannerText: 'REACT'
+        })
+        break;
+      case 2:
+        this.setData({
+          bannerText: 'ANGULAR'
+        })
+        break;
+      case 3:
+        this.setData({
+          bannerText: 'WXMINA'
+        })
+        break;
     }
   },
+
   //事件处理函数
-  bindViewTap: function() {
+  toHome:function(){
+    // 路由到home页
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../../pages/home/home'
     })
   },
   onLoad: function () {
-
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
